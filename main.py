c8adb9ed92848c4a4404d006e2b560425bf43c6e
nadia-al-zaeem <nadiaalzeem@gmail.com>
2026-01-07 23:28:15 +0300

    MAIN CODE

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+'''import os
 import asyncio
 from flask import Flask, request
 from telegram import Update
@@ -43,6 +43,58 @@ async def run_bot():
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 loop.create_task(run_bot())
+
+if __name__ == "__main__":
+    flask_app.run(host="0.0.0.0", port=PORT)'''
+
+
+
+
+import os
+import threading
+from flask import Flask, request
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    filters
+)
+from handlers.start import start, button_handler, message_handler
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.environ.get("BOT_TOKEN")
+WEBHOOK_URL = os.environ.get("RENDER_EXTERNAL_URL")
+PORT = int(os.environ.get("PORT", 5000))
+
+flask_app = Flask(__name__)
+
+telegram_app = ApplicationBuilder().token(TOKEN).build()
+
+# Handlers
+telegram_app.add_handler(CommandHandler("start", start))
+telegram_app.add_handler(CallbackQueryHandler(button_handler))
+telegram_app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
+
+# Webhook endpoint
+@flask_app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.get_json(force=True)
+    update = Update.de_json(data, telegram_app.bot)
+    telegram_app.create_task(telegram_app.process_update(update))
+    return "OK", 200
+
+# تشغيل PTB في Thread منفصل
+def run_bot():
+    telegram_app.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        url_path="webhook",
+        webhook_url=f"{WEBHOOK_URL}/webhook"
+    )
+
+threading.Thread(target=run_bot).start()
 
 if __name__ == "__main__":
     flask_app.run(host="0.0.0.0", port=PORT)
