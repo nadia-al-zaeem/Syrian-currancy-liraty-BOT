c37840dd8c8477445bb94552c891ad1a5679ea0d
nadia-al-zaeem <nadiaalzeem@gmail.com>
2026-01-10 14:31:42 +0300

    MAIN CODE On

--- main.py
+++ main.py
@@ -180,7 +180,6 @@ ApplicationBuilder,CommandHandler,CallbackQueryHandler,MessageHandler, filters
 )
 from handlers.start import start, button_handler, message_handler
 from dotenv import load_dotenv
-from aiohttp import web
 load_dotenv()
 
 TOKEN = os.environ.get("BOT_TOKEN")
@@ -194,19 +193,11 @@ telegram_app.add_handler(CommandHandler("start", start))
 telegram_app.add_handler(CallbackQueryHandler(button_handler))
 telegram_app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
 
-#endpoint
-async def wake(request):
-    print("WAKE REQUEST")
-    print("method:", request.method)
-    print("path:", request.path)
-    print("headers:", dict(request.headers))
-    print("------------------")
-    return web.Response(text="awake")
+
 
 
 if __name__ == "__main__":
-    web_app=web.Application()
-    web_app.router.add_get("/wake",wake)
+    
     telegram_app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
